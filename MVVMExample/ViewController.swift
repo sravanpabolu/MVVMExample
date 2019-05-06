940a1c509d188c42b75b48839be891a4affd1254
Sravan Kumar Pabolu <sravankumar.pabolu@cognizant.com>
2019-05-06 17:54:42 +0530

    Initial Commit

---
+++ MVVMExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMExample
+//
+//  Created by Sravan on 06/05/19.
+//  Copyright © 2019 Sravan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
